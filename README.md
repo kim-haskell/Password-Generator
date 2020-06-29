27259d6bbd67edae099982689ed4022f19896fb7
Kim Haskell <kim.haskell03@gmail.com>
2020-06-28 21:07:48 -0600

    adding details to Readme file and updating Javascript

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Password-Generator
+This password generator will ask you a series of prompts to define criteria you would like to include in your password. Once you select which characters you would like to include, you will then click on the "Generate Password" button and a random password will be generated based on your criteria inputs. 
+
+https://kim-haskell.github.io/Password-Generator/
+
+Author : Kim Haskell
--- script.js
+++ script.js
@@ -1,30 +1,17 @@
    // Assignment Code
    var generateBtn = document.querySelector("#generate");
 
-   // Write password to the #password input
-   function writePassword() {
-     var password = generatePassword();
-     var passwordText = document.querySelector("#password");
-   
-     passwordText.value = password;
-   
-   }
-   
-   // Add event listener to generate button
+    // Write password to the #password input
+    
+    //Add event listener to generate button
    generateBtn.addEventListener("click", writePassword);
 
    
    var passwordCriteria = confirm("Do you want to set the password criteria?");
    
-   if (passwordCriteria) {
-       alert(passwordLength);
-   }
-   else {
-       alert("Cancel request");
-   }
-   
-   var passwordLength = prompt("Please enter the desired length of password, between 8 and 128 characters.");
 
+   var passwordLength = prompt("Please enter the desired length of password, between 8 and 128 characters.");
+   
    
    var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
@@ -34,9 +21,12 @@
 
 
    var confirmLowercase = confirm("Do you want to include lowercase characters?");
-   
+   var confirmUppercase = confirm("Do you want to include uppercase characters?");
+   var confirmNumbers = confirm("Do you want to include numbers?");
+   var confirmSpecialchar = confirm("Do you want to include special characters?");
 
-   var userChars = [];
+
+   var userChars = ["lowercase", "uppercase", "numbers", "specialChar"];
 
 
 
@@ -55,13 +45,14 @@
    if (confirmSpecialchar) {
         userChars = [...userChars, ...specialChar]
    }
-   
-
-   for (var i = 0; i < lowercase.length; i ++) {
-       console.log(lowercase[i]);
-   }
-    for (var i = 0; i < uppercase.length; i ++) {
-        console.log(uppercase[i]);
-    }
-
+   function writePassword() {
+    for (var i = 0; i >= 8; i ++) {
+        var generatePassword = userChars[Math.floor(Math.random) * userChars.length];
     
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+}
